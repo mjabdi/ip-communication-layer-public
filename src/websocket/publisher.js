89089817aadc513121f3e2db00c4951b98899d68
mjabdi <m.jafarabdi@gmail.com>
2019-04-29 11:58:23 +0430

    bugfix

--- src/websocket/publisher.js
+++ src/websocket/publisher.js
@@ -6,7 +6,7 @@ const aesWrapper = require('./../utils/aes-wrapper');
 const bankConnections = require('./bankconnections');
 const redis = require('./../utils/redis');
 
-const io = require('socket.io-emitter')(`redis://:${config.RedisPass}@${config.RedisHost}:${config.RedisPort}`, {
+const io = require('socket.io-emitter')(`redis://:${config.RedisPass}@${config.RedisHost}:${config.RedisPort}/0`, {
     no_ready_check: true,
     auth_pass: config.RedisPass
 });
