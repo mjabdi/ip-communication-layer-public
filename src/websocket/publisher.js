5c511739d496c6b3daa6030c7224b36220313229
mjabdi <m.jafarabdi@gmail.com>
2019-04-29 09:51:56 +0430

    bugfix

--- src/websocket/publisher.js
+++ src/websocket/publisher.js
@@ -6,7 +6,7 @@ const aesWrapper = require('./../utils/aes-wrapper');
 const bankConnections = require('./bankconnections');
 const redis = require('./../utils/redis');
 
-const io = require('socket.io-emitter')(`redis://user:${config.password}@${config.RedisHost}:${config.RedisPort}`);
+const io = require('socket.io-emitter')(`redis://user:${config.RedisPass}@${config.RedisHost}:${config.RedisPort}`);
 
 
 publisher.sendMessage = async (bank, msg) =>
